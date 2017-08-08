5ac50f70032f20b0957bea9e7a25c886bae4e979
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-07 19:05:40 -0500

    add css styling to business card

--- main.js
+++ main.js
@@ -15,31 +15,47 @@ pictureBox.appendChild(image);
 
 // created a place for the basic info to go to go, all are tagged to basics id
 let basics = document.querySelector('#basics');
-let name = document.createElement('li');
-let github = document.createElement('li');
-let email = document.createElement('li');
-let company = document.createElement('li');
-let website = document.createElement('li');
-basics.appendChild(name);
-basics.appendChild(github);
-basics.appendChild(email);
-basics.appendChild(company);
-basics.appendChild(website);
+
+
 
 // called the github API for my git hub and plant ^ elements with proper API info
 function baileyBio() {
   let userInfo = JSON.parse(this.responseText);
   paragraph.innerHTML= `${userInfo.bio}`;
   title.innerHTML = `${userInfo.name}`;
-  name.innerHTML = `Name: ${userInfo.name}`;
-  github.innerHTML = `GitHub URL:${userInfo.html_url}`;
-  email.innerHTML = `Email: ${userInfo.email}`;
-  company.innerHTML = `Company: ${userInfo.company}`;
-  website.innerHTML = `Website: ${userInfo.blog}`;
-  image.setAttribute('src',`${userInfo.avatar_url}`);
-
+  let list = `
+  <li> <span>Name: </span> ${userInfo.name}</li>
+  <li> <span>GitHub URL: </span> <a href=${userInfo.html_url}> Bailey's Github</a> </li>
+  <li> <span>Email: </span> ${userInfo.email}</li>
+  <li> <span>Company: </span> ${userInfo.company} </li>
+  <li> <span> Website: </span><a href=${userInfo.blog}>Company Site</a> </li>
+  `;
+   basics.innerHTML = list;
+   image.setAttribute('src',`${userInfo.avatar_url}`);
 }
+
 let req = new XMLHttpRequest();
 req.addEventListener("load", baileyBio);
 req.open("GET", "https://api.github.com/users/bbryant7");
 req.send();
+
+
+
+
+// let name = document.createElement('li');
+// let github = document.createElement('li');
+// let email = document.createElement('li');
+// let company = document.createElement('li');
+// let website = document.createElement('li');
+// basics.appendChild(name);
+// basics.appendChild(github);
+// basics.appendChild(email);
+// basics.appendChild(company);
+// basics.appendChild(website);
+
+// name.innerHTML = `Name: ${userInfo.name}`;
+// github.innerHTML = `GitHub URL:${userInfo.html_url}`;
+// email.innerHTML = `Email: ${userInfo.email}`;
+// company.innerHTML = `Company: ${userInfo.company}`;
+// website.innerHTML = `Website: ${userInfo.blog}`;
+// image.setAttribute('src',`${userInfo.avatar_url}`);
